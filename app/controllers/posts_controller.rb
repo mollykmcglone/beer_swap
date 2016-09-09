6f6ba6e0ca20d1a302e4e4811dd6ec70a3f2ef9c
Molly <mollykmcglone@gmail.com>
2016-09-09 14:00:24 -0700

    add views and actions to view, edit, delete individual posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,25 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
 private
   def post_params
     params.require(:post).permit(:title, :author, :content, :category_id)
