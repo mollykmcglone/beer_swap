4b38826236e450387daf89674b495ef543a64bc4
Molly <mollykmcglone@gmail.com>
2016-09-16 18:35:14 -0700

    add stock image for seeded posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   validates :title, :content, :presence => true
-  has_attached_file :image, :styles => { :medium => "250x250>", :large => "700x700>"}
+  has_attached_file :image, :styles => { :medium => "250x250>", :large => "700x700>"}, :default_url => "/images/:style/brewers.jpg"
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
   default_scope {order('created_at DESC')}
