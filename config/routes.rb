e312d19084a55036d13b07fb896ce2abd50d50ed
Molly <mollykmcglone@gmail.com>
2016-09-30 12:26:24 -0700

    add/style home page and add beers index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users,
                 controllers: { registrations: "devise/registrations"}
 
-  root :to => 'posts#index'
+  root :to => 'static_pages#home'
+
+  get "/static_pages/:page" => "static_pages#show"
 
   resources :posts do
     resources :comments
@@ -11,5 +13,7 @@ Rails.application.routes.draw do
   resources :profiles do
     resources :beers
   end
+
+  resources :beers, :only => [:index]
 
 end
