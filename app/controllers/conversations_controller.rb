474ee54bed119075811908f9cc9fd3b7878f5e3a
Molly <mollykmcglone@gmail.com>
2016-10-05 17:22:42 -0700

    style conversation page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,8 +7,7 @@ class ConversationsController < ApplicationController
   def create
     recipients = User.where(id: conversation_params[:recipients])
     conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
-    flash[:success] = "Your message was successfully sent!"
-      redirect_to conversation_path(conversation)
+    redirect_to conversation_path(conversation)
   end
 
   def show
