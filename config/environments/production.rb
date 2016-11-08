88b8f2c7d8e11290039616110e11e6b06abc8fa8
Molly <molly@lumenlearning.com>
2016-11-07 16:22:34 -0800

    remove potentially unnecessary paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,5 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     },
-    Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
-    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename',
-    Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
   }
 end
