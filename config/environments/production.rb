fbba9b6bc17a257a2997ee845b83aeaa10c30cfc
Molly <molly@lumenlearning.com>
2016-11-07 21:26:56 -0800

    fix spelling error in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.paperclip.defaults = {
+  config.paperclip_defaults = {
       storage: :s3,
       s3_host_name: 's3-us-west-1.amazonaws.com',
       s3_credentials: {
