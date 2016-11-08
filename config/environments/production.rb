46c73bb0357a80d3839a095a48bf14f67f92cbd7
Molly <molly@lumenlearning.com>
2016-11-07 16:01:42 -0800

    change production config for AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,9 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-    }
+    },
+    Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
+    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename',
+    Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
   }
 end
